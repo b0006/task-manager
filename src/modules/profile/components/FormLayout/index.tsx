f1b408534e62c9c8da225f714d801b249b778d89
b0006 <qsefthuken@gmail.com>
2021-05-23 01:55:11 +0700

    ref(signup): mobile css

--- src/modules/profile/components/FormLayout/index.tsx
+++ src/modules/profile/components/FormLayout/index.tsx
@@ -5,15 +5,9 @@ import styles from './FormLayout.module.scss';
 const FormLayout: React.FC = ({ children }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.form}>
-        {children}
-      </div>
-      <div>
-        or
-      </div>
-      <div>
-        google facebook ...
-      </div>
+      <div className={styles.form}>{children}</div>
+      <div>or</div>
+      <div>google facebook ...</div>
     </div>
   );
 };
